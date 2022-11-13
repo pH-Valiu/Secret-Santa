7f9f63e03931e342d5e6e7c2bdfb358e50cdb5ce
ph-Valiu <sdswae374@gmail.com>
2022-11-14 00:17:29 +0100

    Commit for Version 1.0

--- Secret-Santa/Secret-Santa.pro
+++ Secret-Santa/Secret-Santa.pro
@@ -6,6 +6,9 @@ CONFIG -= app_bundle
 # You can make your code fail to compile if it uses deprecated APIs.
 # In order to do so, uncomment the following line.
 #DEFINES += QT_DISABLE_DEPRECATED_BEFORE=0x060000    # disables all the APIs deprecated before Qt 6.0.0
+LIBS += \
+    "D:\Freizeit\Git-Repositories\Secret-Santa\build-Secret-Santa-Desktop_Qt_6_2_2_MinGW_64_bit-Release\release\QAESEncryption.dll" \
+
 
 SOURCES += \
         main.cpp
@@ -14,3 +17,5 @@ SOURCES += \
 qnx: target.path = /tmp/$${TARGET}/bin
 else: unix:!android: target.path = /opt/$${TARGET}/bin
 !isEmpty(target.path): INSTALLS += target
+
+HEADERS +=
--- Secret-Santa/main.cpp
+++ Secret-Santa/main.cpp
@@ -1,9 +1,55 @@
 #include <QCoreApplication>
+#include "qaesencryption.h"
 
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
-    qDebug("Hello World");
+
+
+    QTextStream out(stdout);
+    QTextStream in(stdin);
+    QString inString;
+    bool checkMark = true;
+
+    out << "How many little santas will be joining the party?\n";
+    out.flush();
+    in.readLineInto(&inString);
+    int santasCount = inString.toInt(&checkMark);
+    QByteArray table[santasCount][2];
+
+    //Filling the table with the names
+    for(int i=0; i<santasCount; i++){
+        out << "Please enter name of santa "<<(i+1)<<": ";
+        out.flush();
+        in.readLineInto(&inString);
+        table[i][0] = inString.toUtf8();
+    }
+
+    //Randomizing the position of the names in the array
+    QByteArray temp;
+    for(int i=0; i<santasCount; i++){
+        int rnd = std::rand() % santasCount;
+        temp = table[i][0];
+        table[i][0] = table[rnd][0];
+        table[rnd][0] = temp;
+    }
+
+    //Encrypting the partner for each with given key in AES_128 in ECB mode with PKCS7 padding and output in base64
+    QAESEncryption crypter(QAESEncryption::AES_128, QAESEncryption::ECB, QAESEncryption::PKCS7);
+    out << "Please enter the key for this secret-santa event (16 Bytes of size): ";
+    out.flush();
+    in.readLineInto(&inString);
+
+    QByteArray outCipher;
+    QByteArray inCipher;
+    for(int i=0; i<santasCount; i++){
+        inCipher = table[(i+1) % santasCount][0];
+        crypter.encode(inCipher, inString.toUtf8(), outCipher);
+        table[i][1] = outCipher.toBase64();
+        out << "Santa "<<table[i][0]<< " has been assigned to gift to: "<<table[i][1]<<"\n";
+        out.flush();
+    }
+
 
     return a.exec();
 }
